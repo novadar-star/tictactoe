14da85dccf370bec3d6319b1891d3215dda45555
novadar-star <jolynekujoismywife06@gmail.com>
2025-12-20 18:36:04 +0800

    Added tictactoe files

---
+++ tictactoe.js
@@ -0,0 +1,14 @@
+//create a 3 by 3 matrix by row and col usig 2d arrays
+
+let items = [
+    [1,2,3],
+    [1,2,3],
+    [1,2,3],
+]
+
+for(const i = 0; i<3; i++){
+    for(const j = 0; j<3; j++){
+    }
+     console.log(" " + items[i][j])
+
+}

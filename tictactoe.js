34573767b2ce4b7c1ca40437d55a0cbba55909c2
novadar-star <jolynekujoismywife06@gmail.com>
2025-12-31 15:45:20 +0800

    Added winning patterns

--- tictactoe.js
+++ tictactoe.js
@@ -1,23 +1,47 @@
 //create a 3 by 3 matrix by row and col usig 2d arrays
 
-let items = [
-    [1,2,3],
-    [1,2,3],
-    [1,2,3],
+const winpatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,4,6],
+    [2,5,8],
+    [3,4,5],
+    [6,7,8]
 ]
-
+   
+/*
 for(const i = 0; i<3; i++){
     for(const j = 0; j<3; j++){
     }
      console.log(" " + items[i][j])
 
 }
-
-const button = document.querySelector("button");
-const img = document.getElementById("image-x")
-
-document.addEventListener("mouseover", mouseover);
-
-function mouseover(){
     
-}
+*/
+const tictacbutton = document.querySelectorAll(".box");
+const text = document.querySelectorAll("h1");
+
+tictacbutton.forEach(button =>{
+    button.addEventListener("click", function(e){
+        e.style.innerText = "X";
+    })
+})
+/*
+tictacbutton.addEventListener("mouseover", e =>{
+    if(e.target.tagName == "button"){
+        e.target.style.backgroundImage = "url(alphabet.png)";
+    }
+});
+
+
+tictacbutton.addEventListener("mouseout", e =>{
+    if(e.target.tagName == "button"){
+        e.target.style.backgroundImage = "url(alphabet.png)";
+    }
+});
+*/
+
+/*image wont appear once hover*/
+

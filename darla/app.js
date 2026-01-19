26f93c628120659a902966bf9423b45ee82c3450
novadar-star <jolynekujoismywife06@gmail.com>
2026-01-19 18:23:15 +0800

    Assigned function to  avariable;

--- darla/app.js
+++ darla/app.js
@@ -1,17 +1,29 @@
 
 //store gameboard array inside of gameboard obect
-const start = () =>{
+
     const Gameboard = () =>{
         const gameboard = ["","","","","","","","",""];
         const board = document.querySelector(".gameboard");
         board.innerHTML = "";
-        gameboard.createElement("div")
+
+        board.forEach((value,index) => {
+            // assign to variable the create elemt
+           // and addclass list
+                const box = board.createElement("div")
+                box.classList.add("box");
+                //box id is base on the index
+                box.id = index;
+                //text conent of box base on X and O
+                box.textContent = value;
+                //append box divs to baord
+                board.appendChild("box")
+
+        })
+        const getgameboard = () => gameboard;
 
     }
-
-
-
-}
+//function forbstarting and displaying boxes
+//function checkwinner/ combination
 
 
 document.querySelector("#start").addEventListener("click", start())

0f57e58e4272276daf375da6250e2016c189fe49
novadar-star <jolynekujoismywife06@gmail.com>
2026-01-19 17:01:39 +0800

    Added gameboard array insdie the object

---
+++ darla/app.js
@@ -0,0 +1,12 @@
+
+//store gameboard array inside of gameboard obect
+const start = () =>{
+    const Gameboard = () =>{
+        const gameboard = ["","","","","",""]
+    }
+
+
+
+}
+
+document.querySelector("#start").addEventListener("click", start)
